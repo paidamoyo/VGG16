15907fbb77c8fe4257451befe9ef54c887ec2383
Dan Salo <dcs41@duke.edu>
2016-10-05 15:19:16 -0400

    add process_VGG image stitching

--- model/process_VGG.py
+++ model/process_VGG.py
@@ -49,9 +49,10 @@ with tf.Session() as sess:
                     if i == 0:
                         image_list.append(volume[i+j*12, :, :, :])
                         continue
-                    print(len(image_list))
+                    additive = volume[i+j*12,:,:,:]
+                    print(additive.shape)
                     print(image_list[j].shape)
-                    image_list[j] = np.concatenate((image_list[j], volume[i+j*12,:,:,:]), axis=0)
+                    image_list[j] = np.concatenate((image_list[j], additive), axis=0)
             for l in range(12):
                 if l == 0:
                     image = image_list[l]
