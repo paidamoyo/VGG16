e039d456bf0d6427974e37bd1b4626e681657b1b
Dan Salo <dcs41@duke.edu>
2016-10-04 23:49:50 -0400

    changed pos_neg; added better reporting metrics

--- model/VGG16.py
+++ model/VGG16.py
@@ -26,6 +26,13 @@ params = {
 }
 
 
+def error_rate(predictions, labels):
+    """Return the error rate based on dense predictions and sparse labels."""
+    return 100.0 - (
+        100.0 *
+        np.sum(np.argmax(predictions, 1) == labels) /
+        predictions.shape[0])
+
 def main():
     image_dict = cp.load(open(flags['aux_directory'] + 'vgg_image_dict.pickle', 'rb'))
     dict_test, dict_train, index_test, index_train = split_data(image_dict)
@@ -36,12 +43,12 @@ def main():
     keep_prob = tf.placeholder(tf.float32)  # dropout (keep probability)
 
     # Construct model
-    model = build_model(x=x, flags=flags, params=params)
+    logits = build_model(x=x, flags=flags, params=params)
 
     # Define loss and optimizer
 
-    cost = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(model, y))
-    # cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(model,y))
+    cost = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits, y))
+    train_prediction = tf.nn.softmax(logits)
     optimizer = tf.train.AdamOptimizer(learning_rate=params['lr']).minimize(cost)
 
     # Evaluate model
@@ -52,23 +59,31 @@ def main():
     init = tf.initialize_all_variables()
     saver = tf.train.Saver()
 
+
+
+
     # Launch the graph
     with tf.Session(config=tf.ConfigProto(log_device_placement=True)) as sess:
         sess.run(init)
         step = 1
+        pos_neg = 1
         while step * params['batch_size'] < params['training_iters']:
-            batch_x, batch_y = generate_minibatch_dict_small(flags['save_directory'], dict_train, 1)
+            batch_x, batch_y = generate_minibatch_dict_small(flags['save_directory'], dict_train, pos_neg)
+            if pos_neg == 1:
+                pos_neg = 0
+            else:
+                pos_neg = 1
 
             sess.run(optimizer, feed_dict={x: batch_x, y: batch_y, keep_prob: params['dropout']})
-            loss, acc = sess.run([cost, accuracy], feed_dict={x: batch_x,
+            loss, acc = sess.run([cost, train_prediction], feed_dict={x: batch_x,
                                                               y: batch_y,
                                                               keep_prob: 1.})
             print("Iter " + str(step * params['batch_size']) + ", Minibatch Loss= " + \
                   "{:.6f}".format(loss) + ", Training Accuracy= " + \
-                  "{:.5f}".format(acc))
+                  'Minibatch error: %.1f%%' % error_rate(acc, batch_y))
             if step % params['display_step'] == 0:
                 # Calculate batch loss and accuracy
-                save_path = saver.save(sess, '../aux/weights/model' + str(step) +'.ckpt')
+                save_path = saver.save(sess, '../aux/model' + str(step) +'.ckpt')
                 print("Model saved in file: %s" % save_path)
             step += 1
         print("Optimization Finished!")
