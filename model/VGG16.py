452e5e4f9000f78ca0b18f825cccd8b056adcabf
Dan Salo <dcs41@duke.edu>
2016-10-04 23:17:12 -0400

    acc

--- model/VGG16.py
+++ model/VGG16.py
@@ -39,14 +39,16 @@ def main():
     model = build_model(x=x, flags=flags, params=params)
 
     # Define loss and optimizer
+
     cost = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(model, y))
+    # cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(model,y))
     optimizer = tf.train.AdamOptimizer(learning_rate=params['lr']).minimize(cost)
 
     # Evaluate model
-    print(model[0][1])
-    print(y[0])
-    accuracy = tf.equal(model[0][tf.to_int32(y)], y[0])
-
+    print(model[0][tf.to_int32(y[0])])
+    correct_pred = tf.equal(tf.argmax(model, 1), tf.argmax(y, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+s
     # Initializing the variables
     init = tf.initialize_all_variables()
     saver = tf.train.Saver()
