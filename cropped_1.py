86898fdc3d9c1b55546cea5cbdbfcf7c84415d45
Dan Salo <dcs41@duke.edu>
2016-10-10 15:10:38 -0400

    finished INbreast processing script for 1_cropped.py

--- cropped_1.py
+++ cropped_1.py
@@ -87,7 +87,7 @@ def process_text_INbreast(flags):
     originals_directory = flags['data_directory'] + 'INbreast' + '/Originals'
     list_dicom_files = find_dicom_files(originals_directory)
     print('Found a total of %d DICOM Images.' % len(list_dicom_files))
-    list_file_pat_names = [(str.split(l, '_')[2], str.split(l, '_')[1]) for l in list_dicom_files]
+    list_file_pat_names = [(str.split(l, '_')[1], str.split(l, '_')[0]) for l in list_dicom_files]
     names = pd.DataFrame(list_file_pat_names)
 
     with open(crosswalk_tsv_path) as tsvfile:
