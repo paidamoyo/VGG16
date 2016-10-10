a8d250c63e31af2246483e2e2f065975f4c00187
Dan Salo <dcs41@duke.edu>
2016-10-10 17:28:33 -0400

    starting on model_3.py

--- functions/tf.py
+++ functions/tf.py
@@ -132,10 +132,10 @@ def load_pretrained_parameters_VGG(flags):
     return weights, biases
 
 
-def model_CNN_FC(x, flags, params):
+def model_CNN_FC(x, params):
     weights, biases = define_parameters()
-    conv2 = cnn2(x=x, weights=weights, biases=biases)
-    logits = fc1(conv2, weights=weights, biases=biases, dropout=params['dropout'])
+    pool2 = cnn2(x=x, weights=weights, biases=biases)
+    logits = fc1(pool2, weights=weights, biases=biases, dropout=params['dropout'])
     return logits
 
 def model_VGG16(x, flags):
