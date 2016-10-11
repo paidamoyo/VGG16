622f42affe56f08b3570244605d42a373b20224a
Dan Salo <dcs41@duke.edu>
2016-10-11 18:23:13 -0400

    printing VGG map stacks

--- cropped_1.py
+++ cropped_1.py
@@ -131,10 +131,8 @@ def main():
     dict_INbreast = [process_text_INbreast(flags) for d in flags['datasets'] if d == 'INbreast']
     image_data_dict = {**dict_SAGE[0], **dict_INbreast[0]}
     example = pd.DataFrame(image_data_dict, index=['ExamNumber', 'View', 'Laterality', 'Filename', 'Label'])
-    pd.set_option('display.multi_sparse', False)
-    print(example)
 
-    if flags['save_pickled_images'] is True:
+    if any(flags['save_pickled_images'], flags['save_processed_jpeg']) is True:
         [process_images(image_data_dict, flags, d) for d in flags['datasets']]
 
     if flags['save_pickled_dictionary'] is True:
