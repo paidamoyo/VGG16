04c6218dbb7aedbba3c8686de7eab9523ecbba61
Dan Salo <dcs41@duke.edu>
2016-10-11 18:25:18 -0400

    printing VGG map stacks

--- cropped_1.py
+++ cropped_1.py
@@ -90,7 +90,6 @@ def process_text_INbreast(flags):
     list_dicom_files = find_dicom_files(originals_directory)
     print('Found a total of %d DICOM Images.' % len(list_dicom_files))
     list_file_pat_names = [(str.split(l, '_')[1], str.split(str.split(l, '_')[0],'/')[-1]) for l in list_dicom_files]
-    print(list_file_pat_names)
     names = pd.DataFrame(list_file_pat_names)
 
     with open(crosswalk_tsv_path) as tsvfile:
