8ebc94ec9330d7c26a9e7c927c1904bb555c3957
Dan Salo <dcs41@duke.edu>
2016-10-12 11:29:44 -0400

    no 64 batch session, too long. Took away one LR too.

--- run_models.sh
+++ run_models.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-for SEED in 1 2 3 4 5 6 7 8 9 10
+for SEED in 2 3 4 5 6 7 8 9 10
 do
-    for LR in 1 2 3 4
+    for LR in 1 2 3
     do
-        for BATCH in 16 32 64
+        for BATCH in 16 32
         do
             python model_3.py $SEED $LR $BATCH
         done
