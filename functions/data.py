9c5cea5db8ba08b02c2b08ee26402b6a80464232
Dan Salo <dcs41@duke.edu>
2016-10-13 09:07:05 -0400

    fixed dataset to include just SAGE

--- functions/data.py
+++ functions/data.py
@@ -20,19 +20,21 @@ def split_data(flags, image_dict, seed, percent_train=0.85):
     for i in range(2):
         dict_test[i] = list()
         dict_train[i] = list()
-    for d in flags['datasets']:
         pdict = pd.DataFrame(image_dict)
-        dict_image = pdict[d]
-        labels = dict_image.iloc[4]
-        for i in range(2):
+        patients = []
+        for d in flags['datasets']:
+            dict_image = pdict[d]
+            labels = dict_image.iloc[4]
             pats = labels[labels == str(i)].index.values
-            patients = np.array([(d, i, j) for (i, j) in pats])
-            partition = int(math.floor(len(patients) * percent_train))  # 70% of data goes to training
-            indexes = np.random.choice(range(len(patients)), size=len(patients))
-            dict_test[i].extend(patients[indexes[partition:]])
-            dict_train[i].extend(patients[indexes[:partition]])
-            index_test.extend(patients[indexes[partition:]])
-            index_train.extend(patients[indexes[:partition]])
+            patients.extend([(d, i, j) for (i, j) in pats])
+        patients = np.array(patients)
+        print(patients)
+        partition = int(math.floor(len(patients) * percent_train))  # 70% of data goes to training
+        indexes = np.random.choice(range(len(patients)), size=len(patients))
+        dict_test[i].extend(patients[indexes[partition:]])
+        dict_train[i].extend(patients[indexes[:partition]])
+        index_test.extend(patients[indexes[partition:]])
+        index_train.extend(patients[indexes[:partition]])
     return dict_train, dict_test, index_train, index_test
 
 
