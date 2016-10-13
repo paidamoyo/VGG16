efcb83ca5f2a895df8a4bea51e6463cc6233858d
Dan Salo <dcs41@duke.edu>
2016-10-13 09:08:02 -0400

    fixed dataset to include just SAGE

--- functions/data.py
+++ functions/data.py
@@ -26,9 +26,10 @@ def split_data(flags, image_dict, seed, percent_train=0.85):
             dict_image = pdict[d]
             labels = dict_image.iloc[4]
             pats = labels[labels == str(i)].index.values
+            print(pats)
+            print(pats.tolist())
             patients.extend([(d, i, j) for (i, j) in pats])
         patients = np.array(patients)
-        print(patients)
         partition = int(math.floor(len(patients) * percent_train))  # 70% of data goes to training
         indexes = np.random.choice(range(len(patients)), size=len(patients))
         dict_test[i].extend(patients[indexes[partition:]])
