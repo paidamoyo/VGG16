8d4b0d5c103e6f2fc167e2fc58f92e085c1b085e
Dan Salo <dcs41@duke.edu>
2016-10-17 16:40:51 -0400

    added unsupervised model

--- functions/tf.py
+++ functions/tf.py
@@ -60,6 +60,8 @@ def deconv2d(x, w, b, stride=2, padding='VALID'):
                                                   filter_width, row_stride, col_stride, padding)
     # batch_size, rows, cols, number of channels #
 
+    print(out_rows)
+    print(out_cols)
     output_shape = tf.pack([tf.shape(x)[0], tf.shape(out_rows), tf.shape(out_cols), tf.shape(w)[2]])
     print(output_shape)
     y = tf.nn.conv2d_transpose(x, w, output_shape, [1, stride, stride, 1], padding)
