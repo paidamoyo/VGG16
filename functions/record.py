ec02d119e0ccddefafa9d0cbc6a7815822cb2e37
Dan Salo <dcs41@duke.edu>
2016-10-17 23:23:29 -0400

    debugging conv_vae

--- functions/record.py
+++ functions/record.py
@@ -33,7 +33,7 @@ def print_log(string, logging):
 
 def record_metrics(loss, acc, batch_y, logging, step, split):
     if step is not None or loss is not None:
-        print_log("Batch Number " + str(step) + ", Image Loss= " + "{!s:.6f}".format(loss), logging)
+        print_log("Batch Number " + str(step) + ", Image Loss= " + "{:.6f}".format(str(loss)), logging)
     if batch_y is None or acc is None:
         print_log(np.squeeze(batch_y), logging)
         print_log(np.argmax(acc, 1), logging)
