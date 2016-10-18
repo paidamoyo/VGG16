632bd06ab255454b9f3b1ae208277c98de8c412b
Dan Salo <dcs41@duke.edu>
2016-10-17 22:53:06 -0400

    debugging conv_vae

--- models/conv_vae.py
+++ models/conv_vae.py
@@ -48,7 +48,7 @@ class ConvVae:
         y = tf.expand_dims(tf.expand_dims(input_sample, 1), 1)
         for d in range(self.num_deconv):
             key = 'deconv' + str(d)
-            y = deconv2d(y, w=self.weights[key], stride=2, padding='VALID')
+            y = deconv2d(y, w=self.weights[key], stride=2, padding='SAME')
         return y, mean, stddev
 
     def encoder(self, x, keep_prob):
