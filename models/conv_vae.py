62f55002ff0223edf8a1ac930bc8b24257298def
Dan Salo <dcs41@duke.edu>
2016-10-19 18:23:54 -0400

    going to replicate tensorflow example VAE_main.py from github with 3 conv layers and 4 deconv layers

--- models/conv_vae.py
+++ models/conv_vae.py
@@ -96,7 +96,8 @@ class ConvVae:
                 previous = self.deconv['input']
             else:
                 previous = self.deconv['layers'][d-1][0]
-            weights['deconv' + str(d)] = deconv_weight_variable('deconv' + str(d), [i[1], i[1], i[0], previous])
+            print(d)
+            weights['deconv' + str(d)] = deconv_weight_variable('deconv' + str(d), [i[1], i[1], previous, i[0]])
             biases['deconv' + str(d)] = bias_variable('deconv' + str(d), [previous])
         return weights, biases
 
@@ -125,8 +126,7 @@ class ConvVae:
         y = tf.expand_dims(tf.expand_dims(input_sample, 1), 1)
         for d in range(self.deconv_num):
             key = 'deconv' + str(d)
-            y = deconv2d(y, w=self.weights[key], b=self.biases[key], stride=self.deconv['layers'][d][2],
-                       padding=self.deconv['layers'][d][3])
+            y = deconv2d(y, w=self.weights[key], b=self.biases[key], stride=self.deconv['layers'][d][2], padding=self.deconv['layers'][d][3])
         return tf.pad(y, [[0, 0], [1, 1], [1, 1], [0, 0]]), mean, stddev
 
     def _create_network(self):
