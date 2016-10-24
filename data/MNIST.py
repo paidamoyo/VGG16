17d2618c87ab3bcd54c8742584424a33d6ac11a7
Dan Salo <dcs41@duke.edu>
2016-10-24 15:55:53 -0400

    Train on normal MNIST with all numbers

--- data/MNIST.py
+++ data/MNIST.py
@@ -9,5 +9,5 @@ def load_data_MNIST():
 
 def generate_MNIST(mnist, batch_size):
     batch = mnist.train.next_batch(batch_size)
-    print(len(batch))
+    print(batch[0].shape)
     batch = np.reshape(batch, [batch_size, 28, 28, 1])
