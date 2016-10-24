afc9fa37b164171b3a40e5336da5e08f00494979
Dan Salo <dcs41@duke.edu>
2016-10-24 15:52:00 -0400

    Train on normal MNIST with all numbers

--- data/MNIST.py
+++ data/MNIST.py
@@ -1,3 +1,4 @@
+import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 
@@ -6,4 +7,4 @@ def load_data_MNIST():
     return mnist
 
 def generate_MNIST(mnist, batch_size):
-    return mnist.train.next_batch(batch_size)
+    return tf.reshape(mnist.train.next_batch(batch_size), [-1,28,28,1])
