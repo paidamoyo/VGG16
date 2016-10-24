08ccd6a4c656d649b193d13939bc34e92bb5e146
Dan Salo <dcs41@duke.edu>
2016-10-24 15:50:10 -0400

    Train on normal MNIST with all numbers

--- data/MNIST.py
+++ data/MNIST.py
@@ -1,7 +1,7 @@
 from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 
-def load_data():
+def load_data_MNIST():
     mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
     return mnist
 
