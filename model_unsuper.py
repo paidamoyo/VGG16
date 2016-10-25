5b3bbf5508ca203a82a5a76d5164d9e129647fed
Dan Salo <dcs41@duke.edu>
2016-10-25 17:33:10 -0400

    New Layers Class

--- model_unsuper.py
+++ model_unsuper.py
@@ -12,14 +12,14 @@ flags = {
     'data_directory': '../../../Data/',  # in relationship to the code_directory
     'aux_directory': 'aux/',
     'model_directory': 'conv_vae/',
-    'datasets': ['MNIST'],
+    'datasets': ['Clutter_MNIST'],
     'restore': False,
     'restore_file': 'starting_point.ckpt',
     'seed': 14,
     'image_dim': 28,
     'hidden_size': 10,
     'batch_size': 128,
-    'display_step': 10,
+    'display_step': 100,
     'lr_iters': [(0.001, 5000), (0.005, 5000), (0.001, 7500), (0.0005, 10000), (0.0001, 10000)]
 }
 
@@ -29,7 +29,7 @@ def main():
     if 'Clutter_MNIST' in flags['datasets']:
         train_set, valid_set, test_set = load_data_cluttered_MNIST(flags['data_directory'] + flags['datasets'][0] + '/mnist.pkl.gz')
         bgf = functools.partial(generate_cluttered_MNIST, dims=[flags['image_dim'], flags['image_dim']],
-                                                      nImages=flags['batch_size'], clutter=0.0, numbers=range(9), prob=1,
+                                nImages=flags['batch_size'], clutter=0.2, numbers=[], prob=1,
                                 train_set=train_set)
     if 'MNIST' in flags['datasets']:
         mnist = load_data_MNIST()
@@ -37,7 +37,7 @@ def main():
     model = ConvVae(flags)
     # print(model.print_variable(var='x_reconst').shape)
 
-    model.train(bgf, lr_iters=flags['lr_iters'], run_num=2)
+    model.train(bgf, lr_iters=flags['lr_iters'], run_num=1)
 
 
 if __name__ == "__main__":
