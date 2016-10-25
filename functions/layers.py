6da5dd698573fc7c5570b82d47f8a46d11a827fa
Dan Salo <dcs41@duke.edu>
2016-10-25 16:06:41 -0400

    New Layers Class

--- functions/layers.py
+++ functions/layers.py
@@ -18,7 +18,7 @@ class Layers:
     def conv2d(self, filter_size, output_channels, stride=1, padding='SAME', activation_fn=tf.nn.relu):
         scope = 'conv_' + str(self.count['conv'])
         input_channels = self.input_shape[3]
-        output_shape = tf.pack([filter_size, filter_size, input_channels, output_channels])
+        output_shape = [filter_size, filter_size, input_channels, output_channels]
         with tf.variable_scope(scope):
             w = conv_weight_variable(name='weights', shape=output_shape)
             b = const_variable(name='bias', shape=[output_channels], value=0.0)
