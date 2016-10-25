6be5d6d7ea48ce9872afbdbf06a6b010ddbfd274
Dan Salo <dcs41@duke.edu>
2016-10-25 16:33:21 -0400

    New Layers Class

--- functions/layers.py
+++ functions/layers.py
@@ -45,6 +45,7 @@ class Layers:
             input_nodes = tf.mul(tf.mul(self.input.get_shape()[1], self.input.get_shape()[2]), self.input.get_shape()[3])
             output_shape = tf.pack([tf.constant(-1), input_nodes])
             self.input = tf.reshape(self.input, output_shape)
+            print(self.input.get_shape())
             if keep_prob != 1:
                 self.input = dropout(self.input, keep_prob=keep_prob)
         self.input_shape = output_shape
