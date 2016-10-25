7217141aef55d5b2c1ecc8ed5f90e343cdbda294
Dan Salo <dcs41@duke.edu>
2016-10-25 16:44:12 -0400

    New Layers Class

--- functions/layers.py
+++ functions/layers.py
@@ -42,7 +42,8 @@ class Layers:
     def flatten(self, keep_prob=1):
         scope = 'flat_' + str(self.count['flat'])
         with tf.variable_scope(scope):
-            input_nodes = tf.mul(tf.constant(9), self.input.get_shape()[3])
+            # input_nodes = tf.mul(tf.constant(9), self.input.get_shape()[3])
+            input_nodes = 3*3*128
             output_shape = tf.pack([-1, input_nodes])
             self.input = tf.reshape(self.input, output_shape)
             if keep_prob != 1:
