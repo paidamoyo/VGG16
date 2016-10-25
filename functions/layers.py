ae7c053cd0679bf71a63680ddff0783b0c8c0651
Dan Salo <dcs41@duke.edu>
2016-10-25 16:22:51 -0400

    New Layers Class

--- functions/layers.py
+++ functions/layers.py
@@ -17,9 +17,9 @@ class Layers:
 
     def conv2d(self, filter_size, output_channels, stride=1, padding='SAME', activation_fn=tf.nn.relu):
         scope = 'conv_' + str(self.count['conv'])
-        input_channels = self.input.get_shape()[3]
-        output_shape = [filter_size, filter_size, input_channels, output_channels]
         with tf.variable_scope(scope):
+            input_channels = self.input.get_shape()[3]
+            output_shape = [filter_size, filter_size, input_channels, output_channels]
             w = conv_weight_variable(name='weights', shape=output_shape)
             b = const_variable(name='bias', shape=[output_channels], value=0.0)
             s = const_variable(name='scale', shape=[output_channels], value=1.0)
@@ -29,9 +29,9 @@ class Layers:
 
     def deconv2d(self, filter_size, output_channels, stride=1, padding='SAME', activation_fn=tf.nn.relu):
         scope = 'deconv_' + str(self.count['deconv'])
-        input_channels = self.input.get_shape()[3]
-        output_shape = [filter_size, filter_size, output_channels, input_channels]
         with tf.variable_scope(scope):
+            input_channels = self.input.get_shape()[3]
+            output_shape = [filter_size, filter_size, output_channels, input_channels]
             w = conv_weight_variable(name='weights', shape=output_shape)
             b = const_variable(name='bias', shape=[output_channels], value=0.0)
             s = const_variable(name='scale', shape=[output_channels], value=1.0)
@@ -42,8 +42,8 @@ class Layers:
     def flatten(self, keep_prob=1):
         scope = 'flat_' + str(self.count['flat'])
         with tf.variable_scope(scope):
-            nodes = tf.mul(tf.mul(self.input.get_shape()[1], self.input.get_shape()[2]) , self.input.get_shape()[3])
-            output_shape = [tf.constant(-1), nodes]
+            input_nodes = tf.mul(tf.mul(self.input.get_shape()[1], self.input.get_shape()[2]), self.input.get_shape()[3])
+            output_shape = [tf.constant(-1), input_nodes]
             self.input = tf.reshape(self.input, output_shape)
             if keep_prob != 1:
                 self.input = dropout(self.input, keep_prob=keep_prob)
@@ -52,9 +52,9 @@ class Layers:
 
     def fc(self, output_nodes, keep_prob=1, activation_fn=tf.nn.relu):
         scope = 'fc_' + str(self.count['fc'])
-        input_nodes = self.input_shape[1]
-        output_shape = tf.pack([input_nodes, output_nodes])
         with tf.variable_scope(scope):
+            input_nodes = self.input.get_shape()[1]
+            output_shape = [input_nodes, output_nodes]
             w = weight_variable(name='weights', shape=output_shape)
             b = const_variable(name='bias', shape=[output_nodes], value=0.0)
             self.input = fc(self.input, w, b, act_fn=activation_fn)
