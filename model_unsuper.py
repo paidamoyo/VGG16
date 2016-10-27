cea7ca31cbd5561701a78546e2b169488f5466f5
Dan Salo <dcs41@duke.edu>
2016-10-27 12:56:10 -0400

    Generate Breast Patches

--- model_unsuper.py
+++ model_unsuper.py
@@ -45,7 +45,7 @@ def main():
         print('Dataset not defined for batch generation')
         exit()
     model = ConvVae(flags)
-    model.x()
+    model.x(bgf)
     # model.train(bgf, lr_iters=flags['lr_iters'], run_num=1)
     # model.x_recon()
 
