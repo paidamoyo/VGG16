d13b7dff3f3bdd8a6432bd06e724467a5e90a0a1
Dan Salo <dcs41@duke.edu>
2016-10-28 13:30:31 -0400

    Test MNIST with recon error

--- model_unsuper.py
+++ model_unsuper.py
@@ -47,7 +47,7 @@ def main():
         print('Dataset not defined for batch generation')
         exit()
     model = ConvVae(flags, run_num=4)
-    model.train(bgf, lr_iters=flags['lr_iters'])
+    model.train(bgf, lr_iters=flags['lr_iters'], run_num=4)
 
 
 if __name__ == "__main__":
