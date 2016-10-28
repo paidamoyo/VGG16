c0446fe61ca260f96912c0888d12813f6a3a563c
Dan Salo <dcs41@duke.edu>
2016-10-28 13:04:10 -0400

    Test MNIST with recon error

--- models/conv_vae.py
+++ models/conv_vae.py
@@ -115,9 +115,9 @@ class ConvVae:
 
     def _create_loss_optimizer(self, epsilon=1e-8):
         #if 'SAGE' in self.flags['datasets']:
-        recon = tf.reduce_sum(tf.squared_difference(self.x, self.x_recon))
+        # recon = tf.reduce_sum(tf.squared_difference(self.x, self.x_recon))
         #else:
-        #    recon = tf.reduce_sum(-self.x * tf.log(self.x_recon + epsilon) - (1.0 - self.x) * tf.log(1.0 - self.x_recon + epsilon))
+        recon = tf.reduce_sum(-self.x * tf.log(self.x_recon + epsilon) - (1.0 - self.x) * tf.log(1.0 - self.x_recon + epsilon))
         vae = tf.reduce_sum(0.5 * (tf.square(self.mean) + tf.square(self.stddev) - 2.0 * tf.log(self.stddev + epsilon) - 1.0))
         cost = tf.reduce_mean(vae + recon)
         optimizer = tf.train.AdamOptimizer(learning_rate=self.lr).minimize(cost)
