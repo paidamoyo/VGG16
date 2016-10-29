e56825b957a7876cc0593ffb1692614a2ac02ad6
Dan Salo <dcs41@duke.edu>
2016-10-29 16:15:04 -0400

    Train on SAGE

--- model_unsuper.py
+++ model_unsuper.py
@@ -30,7 +30,7 @@ flags = {
 
 
 def main():
-
+    print(flags)
     if 'Clutter_MNIST' in flags['datasets']:
         train_set, valid_set, test_set = load_data_cluttered_MNIST(flags['data_directory'] + flags['datasets'][0] + '/mnist.pkl.gz')
         bgf = functools.partial(generate_cluttered_MNIST, dims=[flags['image_dim'], flags['image_dim']],
@@ -48,7 +48,6 @@ def main():
         print('Dataset not defined for batch generation')
         exit()
     model = ConvVae(flags, model=1)
-    model.save_x(bgf)
     model.train(bgf, lr_iters=flags['lr_iters'], model=4)
 
 
