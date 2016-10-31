4230b865e6265c9badea5f45e1eae6ea5c2cc133
Dan Salo <dcs41@duke.edu>
2016-10-31 08:13:34 -0400

    Train on Large Clutter with recon loss

--- models/conv_vae.py
+++ models/conv_vae.py
@@ -132,10 +132,10 @@ class ConvVae:
         return x_recon, mean, stddev, x_gen
 
     def _create_loss_optimizer(self, epsilon=1e-8):
-        if 'SAGE' in self.flags['datasets']:
-            recon = (1 / self.flags['1/sigma2']) * tf.reduce_sum(tf.squared_difference(self.x, self.x_recon))
-        else:
-            recon = tf.reduce_sum(-self.x * tf.log(self.x_recon + epsilon) - (1.0 - self.x) * tf.log(1.0 - self.x_recon + epsilon))
+        # if 'SAGE' in self.flags['datasets']:
+        recon = (1 / self.flags['1/sigma2']) * tf.reduce_sum(tf.squared_difference(self.x, self.x_recon))
+        #else:
+        #    recon = tf.reduce_sum(-self.x * tf.log(self.x_recon + epsilon) - (1.0 - self.x) * tf.log(1.0 - self.x_recon + epsilon))
         vae = tf.reduce_sum(0.5 * (tf.square(self.mean) + tf.square(self.stddev) - 2.0 * tf.log(self.stddev + epsilon) - 1.0))
         cost = tf.reduce_mean(vae + recon)
         optimizer = tf.train.AdamOptimizer(learning_rate=self.lr).minimize(cost)
