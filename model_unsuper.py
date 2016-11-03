f95a0bcd07b8779e69b4b34a27f005bceba9362a
Dan Salo <dcs41@duke.edu>
2016-11-03 12:51:52 -0400

    Try Deconvolution layer

--- model_unsuper.py
+++ model_unsuper.py
@@ -35,7 +35,7 @@ flags = {
 def main():
     o = np.random.randint(1, 1000, 1)
     flags['seed'] = o[0]
-    a = np.random.uniform(-7.5, -5.5, 1)
+    a = np.random.uniform(-5.5, -3.5, 1)
     lr = np.power(10, a[0])
     flags['lr_iters'] = [(lr, 10000)]
     run_num = 1#sys.argv[1]
