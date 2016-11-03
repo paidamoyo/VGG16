96df27277220eaa8cb43c4eadbc0ea70618224e3
Dan Salo <dcs41@duke.edu>
2016-11-02 22:24:42 -0400

    run vae and autocoder

--- run_models.sh
+++ run_models.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-for NUM in {1..60}
+for NUM in {1..25}
 do
-    python model_unsuper.py $NUM
+    python model_unsuper.py ($NUM + 100)
+    python model_conv_auto.py ($NUM + 200)
 done
