9539882c4e6c5b402b9c9ed2e6aa9fb9de2ac8c6
Dan Salo <dcs41@duke.edu>
2016-11-08 14:00:10 -0500

    SAGE 64x64 patches; add more 3x3 filter conv layers and scale up to 128x128

--- models/conv_vae.py
+++ models/conv_vae.py
@@ -105,7 +105,7 @@ class ConvVae:
             stddev = tf.sqrt(tf.exp(stddev))
             input_sample = mean + self.epsilon * stddev
         decoder = Layers(tf.expand_dims(tf.expand_dims(input_sample, 1), 1))
-        decoder.deconv2d(4, 256, padding='VALID')
+        decoder.deconv2d(4, 156, padding='VALID')
         decoder.deconv2d(3, 144, stride=2)
         decoder.deconv2d(3, 128, stride=2)
         decoder.deconv2d(3, 96, stride=2)
@@ -132,7 +132,7 @@ class ConvVae:
 
     def _create_loss_optimizer(self, epsilon=1e-8):
         #if 'SAGE' in self.flags['datasets']:
-        recon = 125*self.flags['image_dim']*self.flags['image_dim'] * tf.reduce_sum(tf.squared_difference(self.x, self.x_recon))
+        recon = 12500000*self.flags['image_dim']*self.flags['image_dim'] * tf.reduce_sum(tf.squared_difference(self.x, self.x_recon))
         #else:
         # recon = (tf.reduce_sum(-self.x * tf.log(self.x_recon + epsilon) - (1.0 - self.x) * tf.log(1.0 - self.x_recon + epsilon)))
         vae = tf.reduce_sum(0.5 * (tf.square(self.mean) + tf.square(self.stddev) - 2.0 * tf.log(self.stddev + epsilon) - 1.0))
