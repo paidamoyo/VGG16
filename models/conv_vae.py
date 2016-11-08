6bd59c2be5b2c8840f5fa4d1d7bf37b1037bd1a6
Dan Salo <dcs41@duke.edu>
2016-11-08 13:55:09 -0500

    SAGE 64x64 patches; add more 3x3 filter conv layers and scale up to 128x128

--- models/conv_vae.py
+++ models/conv_vae.py
@@ -132,7 +132,7 @@ class ConvVae:
 
     def _create_loss_optimizer(self, epsilon=1e-8):
         #if 'SAGE' in self.flags['datasets']:
-        recon = 125000*self.flags['image_dim']*self.flags['image_dim'] * tf.reduce_sum(tf.squared_difference(self.x, self.x_recon))
+        recon = 125*self.flags['image_dim']*self.flags['image_dim'] * tf.reduce_sum(tf.squared_difference(self.x, self.x_recon))
         #else:
         # recon = (tf.reduce_sum(-self.x * tf.log(self.x_recon + epsilon) - (1.0 - self.x) * tf.log(1.0 - self.x_recon + epsilon)))
         vae = tf.reduce_sum(0.5 * (tf.square(self.mean) + tf.square(self.stddev) - 2.0 * tf.log(self.stddev + epsilon) - 1.0))
