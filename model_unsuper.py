9258ca46dc80adc357de45cce6b8f55c3f8552fa
Dan Salo <dcs41@duke.edu>
2016-11-09 12:13:10 -0500

    SAGE 128x128 works; now trying with maxpools

--- model_unsuper.py
+++ model_unsuper.py
@@ -29,8 +29,8 @@ flags = {
 
 
 def main():
-    #o = np.random.randint(1, 1000, 1)
-    flags['seed'] = 324
+    o = np.random.randint(1, 1000, 1)
+    flags['seed'] = o[0]
     # a = np.random.uniform(-5.5, -3.5, 1)
     # lr = 0.0001 #np.power(10, a[0])
     #flags['lr_iters'] = [(lr, 10000)]
