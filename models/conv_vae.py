c347f13d81b1b7883294b7185b12cc4f5e0accf7
Dan Salo <dcs41@duke.edu>
2016-11-10 13:18:29 -0500

    InBreast 128x128, here wego

--- models/conv_vae.py
+++ models/conv_vae.py
@@ -178,13 +178,12 @@ class ConvVae:
         means = list()
         for i in range(num):
             scipy.misc.imsave(self.flags['logging_directory'] + 'x_' + str(i) +'.png', np.squeeze(x[i]))
-            print(x[i].max())
-            print(x[i].min())
-            # means = self.transform(x[1:num, :, :, :])
-            # norm = np.random.normal(loc=means, size=[1, self.flags['hidden_size']])
-        #images = self.sess.run(self.x_gen, feed_dict={self.x: x[1:num, :, :, :], self.keep_prob: 1.0, self.epsilon: norm})
-        #for i in range(len(images)):
-            #scipy.misc.imsave(self.flags['logging_directory'] + 'x_recon' + str(i) + '.png', np.squeeze(images[i]))
+            means = self.transform(x[1:num, :, :, :])
+            print(means)
+            norm = np.random.normal(loc=means, size=[1, self.flags['hidden_size']])
+        images = self.sess.run(self.x_gen, feed_dict={self.x: x[1:num, :, :, :], self.keep_prob: 1.0, self.epsilon: norm})
+        for i in range(len(images)):
+            scipy.misc.imsave(self.flags['logging_directory'] + 'x_recon' + str(i) + '.png', np.squeeze(images[i]))
 
     def transform(self, x):
         """Transform data by mapping it into the latent space."""
