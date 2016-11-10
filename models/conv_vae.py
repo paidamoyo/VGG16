8d1874ca646231deead16614b42fe199f7c4cc4a
Dan Salo <dcs41@duke.edu>
2016-11-09 19:08:35 -0500

    InBreast 128x128, here wego

--- models/conv_vae.py
+++ models/conv_vae.py
@@ -178,9 +178,9 @@ class ConvVae:
         means = list()
         for i in range(num):
             scipy.misc.imsave(self.flags['logging_directory'] + 'x_' + str(1) +'.png', np.squeeze(x[num]))
-            means = self.transform(np.expand_dims(x[0, :, :, :], 0))
+            means = self.transform(x[1:num, :, :, :])
             norm = np.random.normal(loc=means, size=[1, self.flags['hidden_size']])
-        images = self.sess.run(self.x_gen, feed_dict={self.x: np.expand_dims(x[1:num, :, :, :],0), self.keep_prob: 1.0, self.epsilon: norm})
+        images = self.sess.run(self.x_gen, feed_dict={self.x: x[1:num, :, :, :], self.keep_prob: 1.0, self.epsilon: norm})
         for i in range(len(images)):
             scipy.misc.imsave(self.flags['logging_directory'] + 'x_recon' + str(i) + '.png', np.squeeze(images[i]))
 
