9a62da03584d271ea85bd2e0d552d71c30ee6899
Dan Salo <dcs41@duke.edu>
2016-11-09 19:07:46 -0500

    InBreast 128x128, here wego

--- model_unsuper.py
+++ model_unsuper.py
@@ -60,7 +60,7 @@ def main():
     print_log("Seed: %d" % flags['seed'])
     # model_vae.train(bgf, lr_iters=flags['lr_iters'], model=1)
     model_vae.restore()
-    model_vae.save_x_gen(bgf)
+    model_vae.save_x_gen(bgf, 15)
 
 
 
