40b896a270b8e9c93483360fb1f295c6ed99f56e
Dan Salo <dcs41@duke.edu>
2016-11-14 17:48:55 -0500

    run long training set on breast patches; don't go crazy on vae loss; create class for data

--- data/BREAST.py
+++ data/BREAST.py
@@ -10,13 +10,13 @@ class BreastData:
         self.dataset = flags['datasets'][0]
         self.flags = flags
         self.training_num = len(image_dict) - 80
-        breast = self.pdict[self.dataset]
-        self.all_inds = breast.columns.values
+        self.breast = self.pdict[self.dataset]
+        self.all_inds = self.breast.columns.values
         self.data_directory = flags['data_directory'] + self.dataset + '/Preprocessed/' + flags['previous_processed_directory']
 
     def generate_training_batch(self, global_step):
         print(self.all_inds)
-        inds = self.all_inds[global_step % self.training_num][0]
+        inds = self.all_inds[global_step % self.training_num]
         image_path = self.data_directory + check_str(inds[0]) + '_' + check_str(inds[1]) + '.pickle'
         with open(image_path, 'rb') as basefile:
             patches = np.zeros((self.flags['batch_size'], self.flags['image_dim'], self.flags['image_dim'], 1))
