a628c8c5894d31bed5c893469a5c8de1b2d28aa8
Dan Salo <dcs41@duke.edu>
2016-11-14 12:46:01 -0500

    update model_unsuper.py with class

--- model_unsuper.py
+++ model_unsuper.py
@@ -23,7 +23,7 @@ flags = {
     'restore': False,
     'restore_file': 'INbreast.ckpt',
     'recon': 1,
-    'vae': 10000,
+    'vae': 1,
     'image_dim': 128,
     'hidden_size': 128,
     'batch_size': 32,
