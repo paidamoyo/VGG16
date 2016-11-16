f3ecd1e844e45f6f6defa81d66c32f03b52c1e43
Dan Salo <dcs41@duke.edu>
2016-11-16 08:08:53 -0500

    try SAGE images with expanded network, still 128x128, keeping the s_value starting at 1; switch up learning rate

--- conv_vae.py
+++ conv_vae.py
@@ -238,7 +238,7 @@ class ConvVae:
 
 def main():
     o = np.random.randint(1, 1000, 1)
-    flags['seed'] = 107  # o[0]
+    flags['seed'] = 324  # o[0]
     # a = np.random.uniform(-5.5, -3.5, 1)
     # lr = 0.0001 #np.power(10, a[0])
     # flags['lr_iters'] = [(lr, 10000)]
