c82a9b8573c831ef876bb6aa0acadf0e4e17c460
Dan Salo <dcs41@duke.edu>
2016-11-19 17:34:16 -0500

    updated layers package; changed conv_vae to fully convolutional with an average pool; still running on 128x128; now reducing model size

--- conv_vae.py
+++ conv_vae.py
@@ -26,11 +26,11 @@ flags = {
     'vae': 1,
     'image_dim': 128,
     'hidden_size': 128,
-    'batch_size': 32,
+    'batch_size': 16,
     'display_step': 100,
-    'weight_decay': 5e-6,
-    'lr_decay': 0.99,
-    'lr_iters': [(0.00004, 1500), (0.00002, 1500), (0.00001, 1500), (0.00005, 3000), (0.00001, 5000)]
+    'weight_decay': 1e-6,
+    'lr_decay': 0.999,
+    'lr_iters': [(1e-3, 1000), (1e-4, 1000), (1e-5, 1000), (1e-6, 1000)]
 }
 
 
