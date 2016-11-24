e7390d019a7cc633cffd7c413891cbd8ff200749
Dan Salo <dcs41@duke.edu>
2016-11-23 23:51:28 -0500

    adjuted learning rate

--- data/BREAST.py
+++ data/BREAST.py
@@ -1,22 +1,28 @@
 import pickle
 import numpy as np
 import pandas as pd
-from functions.aux import check_str
 
 
 class BreastData:
     def __init__(self, flags, image_dict):
-        self.pdict = pd.DataFrame(image_dict)
-        self.dataset = flags['datasets'][0]
         self.flags = flags
+        self.data_info(image_dict)
+        self.split_data()
+
+    def data_info(self, image_dict):
+        self.pdict = pd.DataFrame(image_dict)
+        self.dataset = self.flags['datasets'][0]
         self.breast = self.pdict[self.dataset]
         self.all_inds = self.breast.columns.values
+        self.data_directory = self.flags['data_directory'] + self.dataset + '/Preprocessed/'\
+                              + self.flags['previous_processed_directory']
+
+    def split_data(self):
         self.training_num = len(self.all_inds) - 40
-        self.data_directory = flags['data_directory'] + self.dataset + '/Preprocessed/' + flags['previous_processed_directory']
 
     def generate_training_batch(self, global_step):
         inds = self.all_inds[global_step % self.training_num]
-        image_path = self.data_directory + check_str(inds[0]) + '_' + check_str(inds[1]) + '.pickle'
+        image_path = self.data_directory + self.check_str(inds[0]) + '_' + self.check_str(inds[1]) + '.pickle'
         with open(image_path, 'rb') as basefile:
             patches = np.zeros((self.flags['batch_size'], self.flags['image_dim'], self.flags['image_dim'], 1))
             labels = np.zeros((self.flags['batch_size']))
@@ -36,3 +42,12 @@ class BreastData:
                 patches[b, :, :, 0] = img - img.mean()
                 labels[b] = label
         return labels, patches
+
+    @staticmethod
+    def check_str(obj):
+        if isinstance(obj, str):
+            return obj
+        if isinstance(obj, float):
+            return str(int(obj))
+        else:
+            return str(obj)
